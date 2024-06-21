aa307d66c5e926e7ef4fa02890eaa1533586a46e
shaoyu chen <chenshaoyu114@gmail.com>
2024-06-21 00:20:05 -0400

    ENV

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const path = require("path");
 const express = require("express");
 const app = express();
 app.use(express.static(path.join(__dirname, "/client/dist")))
-app.listen(3000, ()=> {
+const PORT = process.env.PORT || 80;
+app.listen(PORT, ()=> {
     console.log("Running at http://localhost:3000")
 });
